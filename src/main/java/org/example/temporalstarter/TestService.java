2145bf966bcbe7e04952d3a4e3b905569c1f2445
bradley.davis <bradley.davis@okg.com>
2024-08-27 00:08:40 -0500

    make it work for yun

--- src/main/java/org/example/temporalstarter/TestService.java
+++ src/main/java/org/example/temporalstarter/TestService.java
@@ -2,6 +2,10 @@ package org.example.temporalstarter;
 
 import io.temporal.client.WorkflowClient;
 import io.temporal.client.WorkflowOptions;
+import io.temporal.serviceclient.WorkflowServiceStubs;
+import io.temporal.serviceclient.WorkflowServiceStubsOptions;
+import io.temporal.worker.Worker;
+import io.temporal.worker.WorkerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +21,11 @@ public class TestService {
     @PostConstruct
     public void test() {
         System.out.println("service before");
+
+
+
+
+        // Autoconfigure
         WorkflowInterface workflow = workflowClient.newWorkflowStub(
                 WorkflowInterface.class,
                 WorkflowOptions.newBuilder()
@@ -25,6 +34,37 @@ public class TestService {
                         .build()
         );
         workflow.echoCheck();
+
+
+
+
+        // Old way
+//        WorkflowServiceStubs service = WorkflowServiceStubs.newServiceStubs(WorkflowServiceStubsOptions.newBuilder()
+////                .setTarget("temporal-frontend.dev-com.svc.dev.local")
+//                .setTarget("temporal-frontend.dev-com.svc.dev.local:7233")
+//                .build());
+//
+//        // Workflow client can be used to start, signal, query, cancel, and terminate workflows.
+//        WorkflowClient client = WorkflowClient.newInstance(service);
+//
+//        // Worker factory is used to create Workers that poll specific Task Queues.
+//        WorkerFactory factory = WorkerFactory.newInstance(client);
+//
+//        // Worker that listens on the task queue and hosts both workflow and activity implementations.
+//        Worker worker = factory.newWorker("echo-string");
+//
+//        // Register the workflow implementation with the worker.
+//        worker.registerWorkflowImplementationTypes(WorkflowImpl.class);
+//
+//        // Register the activity implementation with the worker.
+//        worker.registerActivitiesImplementations(new ActivityImpl());
+//
+//        // Start all the workers created by this factory.
+//        factory.start();
+
+
+
+
         System.out.println("service after");
     }
 
